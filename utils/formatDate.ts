9c2b77e8145cb3b7bf386d0013f7c43bc7e8afb2
Hollie Duncan <hollie@contento.io>
2024-10-03 12:14:31 +0100

    Makes date numeric GB

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -2,11 +2,12 @@ export const formatDate = (dateString: string | undefined | null): string => {
   if (dateString) {
     const date = new Date(dateString)
     const options: Intl.DateTimeFormatOptions = {
-      year: 'numeric',
-      month: 'long',
       day: 'numeric',
+      month: 'numeric',
+      year: 'numeric',
+      
     }
-    return new Intl.DateTimeFormat('en-US', options).format(date)
+    return new Intl.DateTimeFormat('en-GB', options).format(date)
   } else {
     return 'Invalid Date'
   }
