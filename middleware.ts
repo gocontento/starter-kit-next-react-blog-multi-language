8e8bbb90aa984adb3de0e2eb9a6bde3244d4d284
Josh Angell <josh@contento.io>
2024-10-04 14:43:39 +0100

    prettier

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,17 @@
-import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
- 
-export default createMiddleware(routing);
- 
+import createMiddleware from 'next-intl/middleware'
+import { routing } from './i18n/routing'
+
+export default createMiddleware(routing)
+
 export const config = {
   // Match only internationalized pathnames
   matcher: [
     // Match root paths
-    '/', '/(fr|en)/:path*',
+    '/',
+    '/(fr|en)/:path*',
     // Match all pathnames except for
     // - … if they start with `/api`, `/_next` or `/_vercel`
     // - … the ones containing a dot (e.g. `favicon.ico`)
     '/((?!api|_next|_vercel|.*\\..*).*)',
-  ]
-};
+  ],
+}
