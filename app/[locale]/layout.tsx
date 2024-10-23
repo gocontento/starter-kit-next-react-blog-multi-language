b031d6023d0cb2bacc0fd2d52d241fd920da2ad5
Hollie Duncan <hollie@contento.io>
2024-10-23 11:44:22 +0100

    removes notFound from layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,7 +5,6 @@ import Footer from '@/components/Footer'
 import { Inter } from 'next/font/google'
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
-import { notFound } from 'next/navigation'
 
 const InterFont = Inter({
   variable: '--font-inter',
@@ -27,26 +26,30 @@ export default async function RootLayout({
 
   if (!mainNavId) {
     throw new Error(
-      'No main nav found. Please ensure you have created one in Contento and copied the ID to your .env file.',
+      'No mainNav ID found - SITE_MAIN_NAV_ID - Please ensure you have created one in Contento and copied the ID to your .env file.',
     )
   }
 
   if (!footerNavId) {
     throw new Error(
-      'No footer nav found. Please ensure you have created one in Contento and copied the ID to your .env file.',
+      'No FooterNav ID found - SITE_FOOTER_NAV_ID - Please ensure you have created one in Contento and copied the ID to your .env file.',
     )
   }
 
   const mainNav = await createClient()
     .getContentById(mainNavId)
     .catch(() => {
-      notFound()
+      throw new Error(
+        'No main nav found in Contento that matches the SITE_MAIN_NAV_ID',
+      )
     })
 
   const footerNav = await createClient()
     .getContentById(footerNavId)
     .catch(() => {
-      notFound()
+      throw new Error(
+        'No footer nav found in Contento that matches the SITE_FOOTER_NAV_ID',
+      )
     })
 
   return (
